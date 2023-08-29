8f3d9652569d3a7ef8e5391b4c84d6ff80302630
Marcus Saraiva <saraivamarcus018@gmail.com>
2023-08-29 17:55:18 -0300

    feat: Adicionado entidade category

---
+++ Store.API/Data/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.API.Data.Entities
+{
+    [Table("tb_category")]
+    public class Category
+    {
+        [Key]
+        [Column("PK_CAT")]
+        public uint Id { get; set; }
+
+        [Required]
+        [Column("ST_CAT_NAME")]
+        public string Name { get; set; }
+    }
+}
