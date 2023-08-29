b7aa29375671d386c32af28825273890e76fa583
Marcus Saraiva <saraivamarcus018@gmail.com>
2023-08-29 18:43:17 -0300

    feat: adicionando entidades Venda e item da venda (selling e sell item)

---
+++ Store.API/Data/Entities/Selling.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Store.API.Data.Entities
+{
+    [Table("tb_selling")]
+    public class Selling
+    {
+        [Key]
+        [Column("PK_SEL")]
+        public uint Id { get; set; }
+
+        [Required]
+        [Column("DB_SEL_PRICE")]
+        public double Price { get; set; }
+        
+        [Required]
+        [Column("DT_SEL_START_AT")]
+        public DateTime StartAt { get; set; }
+        
+        [Required]
+        [Column("DT_SEL_END_AT")]
+        public DateTime EndAt { get; set; }
+
+        public virtual ICollection<SellItem> Items { get; set; }
+    }
+}
